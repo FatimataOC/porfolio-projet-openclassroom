963a3d89a6bb701088728382f6035b7269752a5e
FatimataOC <camarafatimata84@gmail.com>
2023-09-14 14:37:38 +0200

    appel api recuperation travaux pour affichage photo dans modale

--- FrontEnd/modules/modal.js
+++ FrontEnd/modules/modal.js
@@ -18,23 +18,23 @@ if (authUser) {
         window.location.href = "./index.html";
     });
 
-   // Masquage des filtres
-   const buttonFilters = document.querySelector(".button-filter");
-   buttonFilters.classList.add("hidden");
-    // Réglage de marge
-   const worksTitle = document.querySelector(".works_title");
-   worksTitle.classList.add("logged_margin");
-   // La bannière mode édition apparait
-   const banner = document.querySelector(".edit_banner");
-   banner.classList.remove("hidden");
-   // Les deux boutons modifier apparaissent
-   const modifier = document.querySelectorAll(".div_modifier");
-   modifier.forEach(element => {
-       element.classList.remove("hidden");
-   });
-}; 
+// Masquage des filtres
+const sectionFilter = document.querySelector(".button-filter")
+sectionFilter.classList.add("hidden");
+// Réglage de marge
+const worksTitle = document.querySelector(".works_title");
+worksTitle.classList.add("logged_margin");
+// La bannière mode édition apparait
+const banner = document.querySelector(".edit_banner");
+banner.classList.remove("hidden");
+// Les deux boutons modifier apparaissent
+const modifiers = document.querySelectorAll(".div_modifier");
+modifiers.forEach(element => {
+    element.classList.remove("hidden");
+  });
+};
 
-//*modales*//
+/***** MODALES *****/
 
 // Modale galerie - Déclarations
 const openModalBtn = document.querySelectorAll(".modifier");
@@ -43,28 +43,45 @@ const modal = document.querySelector(".modal");
 
 // Modale galerie - Ouverture
 openModalBtn.forEach(function (lien) {
-    lien.addEventListener("click", () => {
-        modal.classList.remove("hidden");
-        modal.removeAttribute("aria-hidden");
-        modal.setAttribute("aria-modal", "true");
-        // Le scroll de l'arrière plan est désactivé
-        document.body.style.overflow = 'hidden';
-        // Modale galerie - Appel des travaux
-  // Modale galerie - Appel des travaux
-  getTravaux().then(response => {
-    recupAltPortfolio(response)
-});
-});
-});
-// Vider le contenu de la modale
-function clearModalContent() {
-  // Supprimer le contenu HTML
-  modalWorksDisplay.innerHTML = "";
-  // Le scroll de l'arrière plan est ré-activé
-  document.body.style.overflow = 'auto';
-};
+lien.addEventListener("click", () => {
+    modal.classList.remove("hidden");  // le modale apparaît
+    modal.removeAttribute("aria-hidden");
+    modal.setAttribute("aria-modal", "true");
+    // Le scroll de l'arrière plan est désactivé
+    document.body.style.overflow = 'hidden';
+   
+// Modale galerie 'récupération des projets via l'API'
+// Déclaration du conteneur des projets pour la modale
+const modalWorksDisplay = document.querySelector(".modal_works");
+    fetch("http://localhost:5678/api/works")
+   .then(response => response.json())
+   .then(data=> {
+      for (let i = 0;i < data.length; i++) {
+       // Création d'un article et apport du contenu dynamique
+       const projet = document.createElement("figure");
+       // Attribution d'une class
+       projet.setAttribute("class", "figure");
+       // Ajout d'une image
+       const image = document.createElement("img");
+       image.src = data[i].imageUrl;
+        // Ajout icone 'supprimer'
+        const trash = document.createElement("div");
+        trash.setAttribute("class", "trash");
+        trash.innerHTML = `<i class="fa-regular fa-trash-can" aria-hidden="true"></i>`;
+       // Ajout d'un sous-titre
+       const subtitle = document.createElement("figcaption");
+       subtitle.innerText = "éditer";
+       // Association Enfant-Parent
+       modalWorksDisplay.appendChild(projet);
+       projet.appendChild(image);
+       projet.appendChild(subtitle);
+       projet.appendChild(trash);
+      }
+    })
+   });
+  });
 
-// Modale galerie - Fermeture - Croix
+// Fermeture de la croix - modale gallerie
 closeModalBtn.forEach(function (lien) {
   lien.addEventListener("click", () => {
       modal.classList.add("hidden");
@@ -73,8 +90,8 @@ closeModalBtn.forEach(function (lien) {
       clearModalContent();
   });
 });
-
-// Modale galerie - Fermeture - En dehors
+  
+//fermeture en dehors - Modale galerie 
 function closeModalOutside(event) {
   // Si le clic est en dehors du contenu de la modale
   if (event.target === modal) {
@@ -84,3 +101,8 @@ function closeModalOutside(event) {
       clearModalContent();
   };
 };
+
+// Fermeture au clic en dehors de la modale
+window.addEventListener('click', closeModalOutside);
+
+// masquage des filtres
